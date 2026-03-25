25a3d58b4e62fe526f6c5cfb909ef4006d4a5603
laxmanreddysaddi <laxmanreddy7780269265@gmail.com>
2026-03-25 15:36:19 +0000

    Prepared frontend for Vercel deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://documind-backend-30m4.onrender.com/api";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 // Helper
 const getToken = () => localStorage.getItem("token");
