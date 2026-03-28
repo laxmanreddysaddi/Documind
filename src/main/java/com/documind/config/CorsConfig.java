9ee5bffa5f779135934604cf5217ed7d3dd03129
laxmanreddysaddi <laxmanreddy7780269265@gmail.com>
2026-03-28 13:31:03 +0000

    Fix CORS + 403 error

--- src/main/java/com/documind/config/CorsConfig.java
+++ src/main/java/com/documind/config/CorsConfig.java
@@ -2,22 +2,34 @@ package com.documind.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.cors.*;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
+    public CorsConfigurationSource corsConfigurationSource() {
 
-                registry.addMapping("/**")
-                        .allowedOrigins("*")   // frontend URL
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://documind-l6pp3uz0v-laxmanreddysaddis-projects.vercel.app"
+        ));
+
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        config.setAllowedHeaders(List.of("*"));
+
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
